315f67a094a4166342a33845a91d2a29eaec717c
basyura <basyura@gmail.com>
2011-07-10 14:34:10 +0900

    first commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace TwitterClient
+{
+    public partial class MainPage : UserControl
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            WebClient twitterclient = new WebClient();
+            twitterclient.DownloadStringCompleted +=
+                new DownloadStringCompletedEventHandler(twitterclient_DownloadStringCompleted);
+            twitterclient.DownloadStringAsync(
+                new Uri("http://api.twitter.com/1/statuses/user_timeline.xml?screen_name=" + UserName.Text));
+        }
+        void twitterclient_DownloadStringCompleted(object sendar, DownloadStringCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                return;
+            }
+            XElement xmlTweets = XElement.Parse(e.Result);
+            listBox1.ItemsSource =
+                from tweet in xmlTweets.Descendants("status")
+                select new TwitterItems
+                {
+                    ImageSource =
+                        tweet.Element("user").Element("profile_image_url").Value,
+                    Message = tweet.Element("text").Value,
+                    UserName = tweet.Element("user").Element("screen_name").Value
+                };
+        }
+    }
+}
---
+++ TwitterItems.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace TwitterClient
+{
+    public class TwitterItems
+    {
+        public string UserName    { get; set; }
+        public string Message     { get; set; }
+        public string ImageSource { get; set; }
+    }
+}
